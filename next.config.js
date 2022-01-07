be45f483eaf64854412699884b2fde946fec935b
Mahdi Rezayi <mahdi.rezzai93@gmail.com>
2022-01-07 22:06:26 +0330

    1234. Starting BookIT Project with Next.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    DB_LOCAL_URI:
+      'mongodb+srv://mh77-1234:mh77-1234@mh77-media.wuupo.mongodb.net/bookit?retryWrites=true&w=majority',
+  },
+}
