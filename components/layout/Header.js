dd59b6b5bab45d1514c668d7b0e48ede6a30fad1
Mahdi Rezayi <mahdi.rezzai93@gmail.com>
2022-01-29 16:40:20 +0330

    12. Authentication with next-auth

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Image from 'next/image'
 import Link from 'next/link'
+import { useSelector, useDispatch } from 'react-redux'
+import { signOut } from 'next-auth/client'
+
+import { loadUser } from '../../redux/actions/userActions'
 
 const Header = () => {
+  const dispatch = useDispatch()
+
+  const { user, loading } = useSelector((state) => state.auth)
+
+  useEffect(() => {
+    dispatch(loadUser())
+  }, [dispatch])
+
+  const logoutHandler = () => {
+    signOut()
+  }
+
   return (
     <nav className='navbar row justify-content-center sticky-top'>
       <div className='container'>
@@ -21,9 +38,58 @@ const Header = () => {
         </div>
 
         <div className='col-3 mt-3 mt-md-0 text-center'>
-          <a className='btn btn-danger px-4 text-white login-header-btn float-right'>
-            Login
-          </a>
+          {user ? (
+            <div className='ml-4 dropdown d-line'>
+              <a
+                className='btn dropdown-toggle mr-4 d-flex align-items-center'
+                id='dropDownMunuButton'
+                data-toggle='dropdown'
+                aria-haspopup='true'
+                aria-expanded='false'
+              >
+                <figure className='avatar avatar-nav'>
+                  <Image
+                    src={user.avatar && user.avatar.url}
+                    alt={user && user.name}
+                    width={150}
+                    height={150}
+                    className='rounded-circle'
+                  />
+                </figure>
+                <span>{user && user.name}</span>
+              </a>
+
+              <div
+                className='dropdown-menu'
+                aria-labelledby='dropDownMunuButton'
+              >
+                <Link href={'/bookings/me'}>
+                  <a className='dropdown-item'>My Bookings</a>
+                </Link>
+
+                <Link href={'/me/update'}>
+                  <a className='dropdown-item'>Profile Options</a>
+                </Link>
+
+                <Link href={'/'}>
+                  <a
+                    className='dropdown-item text-danger'
+                    onClick={logoutHandler}
+                  >
+                    Logout
+                  </a>
+                </Link>
+              </div>
+            </div>
+          ) : (
+            !loading && (
+              <Link href={'/login'}>
+                <a className='btn btn-danger px-4 text-white login-header-btn float-right'>
+                  Login
+                </a>
+              </Link>
+            )
+          )}
         </div>
       </div>
     </nav>
--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ module.exports = {
   env: {
     DB_LOCAL_URI:
       'mongodb+srv://mh77-1234:mh77-1234@mh77-media.wuupo.mongodb.net/bookit?retryWrites=true&w=majority',
+    CLOUDINARY_CLOUD_NAME: 'dmscqygte',
+    CLOUDINARY_API_KEY: '938881393821676',
+    CLOUDINARY_API_SECRET: 'JjWNA1SrLm9n172TY4FETyU05zA',
   },
   images: {
     domains: ['res.cloudinary.com'],
